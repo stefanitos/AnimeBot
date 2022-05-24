a0576226d9f7ce2014ee75e88e531e93ccd1295a
bacanater <65655225+bacanater@users.noreply.github.com>
2022-05-24 20:27:52 +0200

    yes

--- app.py
+++ app.py
@@ -56,32 +56,36 @@ async def anime(ctx,command,*anime_title):
             else:
                 await firstmsg.edit(content="***Found the following anime:***\n" + animestring)
             
-            secondmsg = await ctx.send("Please enter the number of the anime you would like to add")
+                secondmsg = await ctx.send("Please enter the number of the anime you would like to add")
 
-            def check(m):
-                return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
+                def check(m):
+                    return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
 
-            try:
-                msg = await bot.wait_for('message', check=check, timeout=30.0)
-            except asyncio.TimeoutError:
-                await secondmsg.edit(content="Timed out")
-                return
-            else:
-                num = int(msg.content) - 1
-                if num < 1 or num > len(animelist):
-                    await secondmsg.edit(content="Please enter a valid number")
+                try:
+                    msg = await bot.wait_for('message', check=check, timeout=30.0)
+                except asyncio.TimeoutError:
+                    await secondmsg.edit(content="Timed out")
                     return
                 else:
+                    num = int(msg.content) - 1
                     anime = animelist[num]
                     if anime in root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]:
                         await secondmsg.edit(content="***Anime already in list***")
                         return
                     else:
                         root.get_collection("users").update_one({"id": ctx.author.id}, {"$push": {"anime_list": anime}})
-                        await secondmsg.edit(content="***Anime added!***")
+                        await secondmsg.edit(content="***Anime : " + anime + " added to list!***")
                         return
 
-                
+    elif command == "list":
+        if root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"] == []:
+            await ctx.send("***No anime in list***")
+        else:
+            anime_list = root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]
+            animestring = ""
+            for anime in anime_list:
+                animestring += anime + "\n"
+            await ctx.send("***" + ctx.author.name + "'s Anime List:***\n" + animestring)           
 
 
 bot.run('NjI1MzE5NjU4NjQ3NDUzNzE3.GfsL5h.7KgA2DfdCnrhL1BCZCHkqwn0dJzZUj5l_ZdRCg')
