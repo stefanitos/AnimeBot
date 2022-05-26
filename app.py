7204b7e4cb2d4d3a845ac6f82d4bd6165576eee4
bacanater <65655225+bacanater@users.noreply.github.com>
2022-05-26 14:13:26 +0200

    delete anime title from list

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from time import sleep
 import aiohttp
 import pymongo
 from bs4 import BeautifulSoup
@@ -69,13 +70,37 @@ async def anime(ctx,command,*anime_title):
                 else:
                     num = int(msg.content) - 1
                     anime = animelist[num]
+
                     if anime in root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]:
                         await secondmsg.edit(content="***Anime already in list***")
                         return
                     else:
-                        root.get_collection("users").update_one({"id": ctx.author.id}, {"$push": {"anime_list": anime}})
+                        URL = "https://gogoanime.gg/category/" + anime
+                        HEADER = ({'User-Agent':
+                                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                                'Accept-Language': 'en-US, en;q=0.5'})
+
+                        async with aiohttp.ClientSession() as session:
+                            async with session.get(URL, headers=HEADER) as response:
+                                data = await response.text()
+
+                        ul = BeautifulSoup(data, 'html.parser').find('ul', id='episode_page').find_all("li")
+                        temp = []
+                        latest = 0
+
+                        for item in ul:
+                            temp.append(item.find("a").text)
+
+                        if temp == ['0']:
+                            return
+                        elif temp.__len__() > 1:
+                            latest = int(temp[-1].split("-")[1])
+                        else:
+                            latest = int(temp[0].split("-")[1])
+                        
+                        root.get_collection("users").update_one({"id": ctx.author.id}, {"$push": {"anime_list": [anime,latest]}})
                         await secondmsg.edit(content="***Anime : " + anime + " added to list!***")
-                        return
 
     elif command == "list":
         if root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"] == []:
@@ -84,8 +109,38 @@ async def anime(ctx,command,*anime_title):
             anime_list = root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]
             animestring = ""
             for anime in anime_list:
-                animestring += anime + "\n"
+                animestring += anime[0] + "\n"
             await ctx.send("***" + ctx.author.name + "'s Anime List:***\n" + animestring)           
 
+    elif command == "remove":
+        if root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"] == []:
+            await ctx.send("***No anime in list***")
+        else:
+            anime_list = root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]
+            animestring = ""
+            i = 1
+            for anime in anime_list:
+                animestring += str(i) + ") " + anime[0] + "\n"
+                i += 1
+            await ctx.send("***" + ctx.author.name + "'s Anime List:***\n" + animestring)
+
+            secondmsg = await ctx.send("Please enter the number of the anime you would like to remove")
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
+
+            try:
+                msg = await bot.wait_for('message', check=check, timeout=30.0)
+            except asyncio.TimeoutError:
+                await secondmsg.edit(content="Timed out")
+                return
+            else:
+                num = int(msg.content) - 1
+                anime = anime_list[num]
+
+                anime_list = root.get_collection("users").find_one({"id": ctx.author.id})["anime_list"]
+
+                root.get_collection("users").update_one({"id": ctx.author.id}, {"$pull": {"anime_list": anime}})
+                await secondmsg.edit(content="***Anime : " + anime[0] + " removed from list!***")
 
 bot.run('NjI1MzE5NjU4NjQ3NDUzNzE3.GfsL5h.7KgA2DfdCnrhL1BCZCHkqwn0dJzZUj5l_ZdRCg')
