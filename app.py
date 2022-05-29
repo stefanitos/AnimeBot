592a5609518ae9d1086567fd14d11d595507f02c
bacanater <65655225+bacanater@users.noreply.github.com>
2022-05-29 12:48:06 +0200

    dsadsad

--- app.py
+++ app.py
@@ -29,85 +29,77 @@ async def on_member_join(member):
         new_channel = await server.create_text_channel(name=str(member.id))
         await new_channel.set_permissions(role, read_messages=True, send_messages=True, read_message_history=True)
         await new_channel.set_permissions(server.default_role, read_messages=False)
-        await member.add_roles(role)
-    else:
-        await member.add_roles(role)
+    await member.add_roles(role)
     
 
 @bot.command()
-async def add(ctx,*,animename):
+async def add(ctx,*animename):
     """Adds an anime to your anime list"""
     check_user(ctx.author)
-
-    if animename == "":
-        await ctx.send("Please enter an anime name")
+    if animename.__len__() == 0:
+        await ctx.send("Please specify an anime name")
+        return
+    anime_name = " ".join(animename)
+    firstmsg = await ctx.send("Searching for anime...")
+    url = 'https://gogoanime.sk/search.html?keyword='
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url + anime_name) as response:
+            data = await response.text()
+    soup = BeautifulSoup(data, 'html.parser')
+    results = soup.find_all("p",{"class": "name"})
+    animelist = []
+    animestring = ""
+    counter = 1
+    for elements in results:
+        animelist.append(elements.find("a")["href"].split("/category/")[1])
+    for anime in animelist:
+        animestring += str(counter) + ") " + anime + "\n"
+        counter += 1
+    if animestring == "":
+        await firstmsg.edit(content="No anime found with title: " + anime_name)
     else:
-        firstmsg = await ctx.send("Searching for anime...")
-        url = 'https://gogoanime.sk/search.html?keyword='
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url + animename) as response:
-                data = await response.text()
-        soup = BeautifulSoup(data, 'html.parser')
-        results = soup.find_all("p",{"class": "name"})
-        animelist = []
-        animestring = ""
-        counter = 1
-        for elements in results:
-            animelist.append(elements.find("a")["href"].split("/category/")[1])
-        
-        for anime in animelist:
-            animestring += str(counter) + ") " + anime + "\n"
-            counter += 1
-        
-        if animestring == "":
-            await firstmsg.edit(content="No anime found with title: " + animename)
+        await firstmsg.edit(content="***Found the following anime:***\n" + animestring)
+        secondmsg = await ctx.send("\nPlease enter the number of the anime you would like to add")
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
+        try:
+            msg = await bot.wait_for('message', check=check, timeout=30.0)
+        except asyncio.TimeoutError:
+            await secondmsg.edit(content="Timed out")
+            return
         else:
-            await firstmsg.edit(content="***Found the following anime:***\n" + animestring)
-        
-            secondmsg = await ctx.send("\nPlease enter the number of the anime you would like to add")
-            def check(m):
-                return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
-            try:
-                msg = await bot.wait_for('message', check=check, timeout=30.0)
-            except asyncio.TimeoutError:
-                await secondmsg.edit(content="Timed out")
+            num = int(msg.content) - 1
+            anime = animelist[num]
+            if anime in ROOT.find_one({"id": ctx.author.id})["anime_list"]:
+                await secondmsg.edit(content="***Anime already in list***")
                 return
             else:
-                num = int(msg.content) - 1
-                anime = animelist[num]
-
-                if anime in ROOT.find_one({"id": ctx.author.id})["anime_list"]:
-                    await secondmsg.edit(content="***Anime already in list***")
+                URL = "https://gogoanime.gg/category/" + anime
+                HEADER = ({'User-Agent':
+                        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                        (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                        'Accept-Language': 'en-US, en;q=0.5'})
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(URL, headers=HEADER) as response:
+                        data = await response.text()
+                ul = BeautifulSoup(data, 'html.parser').find('ul', id='episode_page').find_all("li")
+                temp = []
+                latest = 0
+                for item in ul:
+                    temp.append(item.find("a").text)
+                if temp == ['0']:
                     return
+                elif temp.__len__() > 1:
+                    latest = int(temp[-1].split("-")[1])
                 else:
-                    URL = "https://gogoanime.gg/category/" + anime
-                    HEADER = ({'User-Agent':
-                            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                            (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
-                            'Accept-Language': 'en-US, en;q=0.5'})
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(URL, headers=HEADER) as response:
-                            data = await response.text()
-                    ul = BeautifulSoup(data, 'html.parser').find('ul', id='episode_page').find_all("li")
-                    temp = []
-                    latest = 0
-                    for item in ul:
-                        temp.append(item.find("a").text)
-                    if temp == ['0']:
-                        return
-                    elif temp.__len__() > 1:
-                        latest = int(temp[-1].split("-")[1])
-                    else:
-                        latest = int(temp[0].split("-")[1])
-                    
-                    ROOT.update_one({"id": ctx.author.id}, {"$push": {"anime_list": [anime,latest]}})
-
-                    # if in animelist there is no anime, add it
-                    if ANIMELIST.find_one({"anime": anime}) == None:
-                        ANIMELIST.insert_one({"anime": anime, "users": [ctx.author.id],"latest": latest})
-                    else:
-                        ANIMELIST.update_one({"anime": anime}, {"$push": {"users": ctx.author.id}})
-                    await secondmsg.edit(content="***Anime : " + anime + " added to list!***")
+                    latest = int(temp[0].split("-")[1])
+                ROOT.update_one({"id": ctx.author.id}, {"$push": {"anime_list": [anime,latest]}})
+                # if in animelist there is no anime, add it
+                if ANIMELIST.find_one({"anime": anime}) == None:
+                    ANIMELIST.insert_one({"anime": anime, "users": [ctx.author.id],"latest": latest})
+                else:
+                    ANIMELIST.update_one({"anime": anime}, {"$push": {"users": ctx.author.id}})
+                await secondmsg.edit(content="***Anime : " + anime + " added to list!***")
 
 @bot.command()
 async def list(ctx,*args):
@@ -139,10 +131,11 @@ async def remove(ctx):
     else:
         anime_list = ROOT.find_one({"id": ctx.author.id})["anime_list"]
         animestring = ""
+        counter = 1
         for anime in anime_list:
-            animestring += anime[0] + "\n"
+            animestring += str(counter) + ") " + anime[0] + "\n"
         await ctx.send("***" + ctx.author.name + "'s Anime List:***\n" + animestring)
-        await ctx.send("Please enter the number of the anime you would like to remove")
+        firstmsg = await ctx.send("Please enter the number of the anime you would like to remove")
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
@@ -157,8 +150,12 @@ async def remove(ctx):
             anime = anime_list[num]
 
             ROOT.update_one({"id": ctx.author.id}, {"$pull": {"anime_list": anime}})
-            ANIMELIST.update_one({"anime": anime[0]}, {"$pull": {"users": ctx.author.id}})
-            await ctx.send("***Anime : " + anime[0] + " removed from list!***")
+            # if there is no other users in the list, remove the anime
+            if ANIMELIST.find_one({"anime": anime[0]})["users"].__len__() == 1:
+                ANIMELIST.delete_one({"anime": anime[0]})
+            else:
+                ANIMELIST.update_one({"anime": anime[0]}, {"$pull": {"users": ctx.author.id}})
+            await firstmsg.edit(content="***Anime : " + anime[0] + " removed from list!***")
 
 def check_user(user):
     if ROOT.find_one({"id": user.id}) == None:
