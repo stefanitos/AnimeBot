5f9c2e0feb74e59612c14d2601b0b72aeee36289
bacanater <65655225+bacanater@users.noreply.github.com>
2022-06-07 13:07:07 +0200

    s

--- app.py
+++ app.py
@@ -231,10 +231,10 @@ async def info(ctx):
     """system info"""
     if not debug:
         await ctx.send(
-        "CPU Usage: " + str(psutil.cpu_percent()) + "%\n" +
-        "CPU Temp: " + str(psutil.sensors_temperatures()['cpu_thermal'][0][1]) + "°C\n" +
-        "RAM Usage: " + str(psutil.virtual_memory().percent) + "%\n" +
-        "Disk Usage: " + str(psutil.disk_usage('/').percent) + "%"
+        "CPU Usage: ***" + str(psutil.cpu_percent()) + "***%\n" +
+        "CPU Temp: ***" + str(psutil.sensors_temperatures()['cpu_thermal'][0][1]) + "***°C\n" +
+        "RAM Usage: ***" + str(psutil.virtual_memory().percent) + "***%\n" +
+        "Disk Usage: ***" + str(psutil.disk_usage('/').percent) + "***%"
         )
 
 
